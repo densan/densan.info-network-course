ff722594435568e08a248671ae8c5b4855808a88
ww24 <ww24gm@gmail.com>
2013-07-09 19:54:52 +0900

    update JavaScript/02

---
+++ JavaScript/02/script.js
@@ -0,0 +1,32 @@
+/**
+ * JavaScript 基礎 Ⅱ 課題
+ */
+
+/** 
+ * 問1: オブジェクトをブラウザのコンソールに列挙する関数を定義する。
+ * prototype も考慮すること (obj に属するプロパティだけを列挙する)
+ * 返り値はオブジェクトの要素数とする。
+ */
+function showObject(obj) {
+  
+}
+
+/**
+ * 問2: 下記の makeCountUp 関数にコードを追加し、
+ * 呼び出す度に 1 ずつ大きな値を返す関数を生成する関数を作成する。
+ */
+function makeCountUp() {
+  var count = 0;
+  return function () {
+    return count;
+  };
+}
+
+/**
+ * 問3: 制御構文・関数などを自由に使って、
+ * 引数で与えられた配列の要素(数値)の中から、
+ * 偶数だけを抜き出して新しい配列を返り値とする関数を作成する。
+ */
+function getEvenArray(arr) {
+  
+}
