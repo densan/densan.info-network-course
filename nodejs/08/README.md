49d4ee4d0259af6b7464e37d8f3cfd6766eab6ac
ww24 <ww24gm@gmail.com>
2013-11-26 16:36:56 +0900

    add nodejs/08

---
+++ nodejs/08/README.md
@@ -0,0 +1,12 @@
+Code Coverage Tool
+==================
+
+コードカバレッジを計測します。
+
+setup
+-----
+npm install
+
+test
+----
+npm test
---
+++ nodejs/08/test/factorial.js
@@ -0,0 +1,40 @@
+// factorial.js
+
+if (typeof window === "object") {
+  var expect = chai.expect;
+} else {
+  var expect = require("chai").expect;
+  var factorial = require("../factorial");
+}
+
+describe("factorial", function () {
+  it("argument: 0", function () {
+    var f = factorial(0);
+    expect(f).to.equal(1);
+  });
+
+  it("argument: 1", function () {
+    var f = factorial(1);
+    expect(f).to.equal(1);
+  });
+
+  it("argument: 2", function () {
+    var f = factorial(2);
+    expect(f).to.equal(2);
+  });
+
+  it("argument: 10", function () {
+    var f = factorial(10);
+    expect(f).to.equal(3628800);
+  });
+
+  it("argument: -1", function () {
+    var f = factorial.bind(null, -1);
+    expect(f).to.throw(RangeError, "n must be a natural number");
+  });
+
+  it("argument: null", function () {
+    var f = factorial.bind(null, null);
+    expect(f).to.throw(TypeError, "n must be a natural number");
+  });
+});
